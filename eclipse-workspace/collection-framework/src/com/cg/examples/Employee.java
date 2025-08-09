d1f15f57c8ecdc23cd8420c16ed0a06da3ca7384
Akshay Singh <singhakshay8081@gmail.com>
2025-08-10 00:44:08 +0530

    mulesoft integration

---
+++ eclipse-workspace/collection-framework/src/com/cg/examples/Employee.java
@@ -0,0 +1,97 @@
+package com.cg.examples;
+
+import java.time.LocalDate;
+
+public class Employee {
+
+	private int employeeId;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private LocalDate hireDate;
+	private double salary;
+	Department dept;
+	
+	public Employee() {
+	}
+
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public LocalDate getHireDate() {
+		return hireDate;
+	}
+
+	public void setHireDate(LocalDate hireDate) {
+		this.hireDate = hireDate;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public Employee(int employeeId, String firstName, String lastName, String email, LocalDate hireDate,
+			double salary, Department dept) {
+		super();
+		this.employeeId = employeeId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.hireDate = hireDate;
+		this.salary = salary;
+		this.dept=dept;
+	}
+
+	public Department getDept() {
+		return dept;
+	}
+
+	public void setDept(Department dept) {
+		this.dept = dept;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [employeeId=" + employeeId + ", firstName=" + firstName + ", lastName=" + lastName + ", email="
+				+ email + ", hireDate=" + hireDate + ", salary=" + salary + ", dept=" + dept + "]";
+	}
+
+	
+	
+	
+	
+	
+}
